395df4da52daac1e5ccf3a31ca2b7ae459f36dbf
Alex1iv <ivanov.politehnik@gmail.com>
2022-12-18 00:53:43 +0600

    import with pydot

--- utils/reader_config.py
+++ utils/reader_config.py
@@ -5,6 +5,6 @@ def read_config(path_to_json_conf):
     with open(path_to_json_conf, 'r') as config_file:
         config_dict = json.load(config_file)
     
-    #config = DotMap(config_dict)
+    config = DotMap(config_dict)
     
-    return config_dict
+    return config #config_dict
