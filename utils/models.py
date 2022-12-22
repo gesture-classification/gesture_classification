583d0f99b0a64ba23480f08f10906ac05487839f
Alex1iv <ivanov.politehnik@gmail.com>
2022-12-22 12:21:08 +0600

    add compile

--- utils/models.py
+++ utils/models.py
@@ -6,11 +6,11 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from keras import layers
 
 # Импорт параметров
-from utils.functions import config_reader
+from utils.functions import config_reader, f1
 
 config = config_reader() #'../config/data_config.json'
 
-class simpleRNN():
+class SimpleRNN():
     """Класс создаёт модель simpleRNN.
     Параметры:
     ----------
@@ -20,13 +20,14 @@ class simpleRNN():
     units - размерность модели   
     """    
     def __init__(self, X_train_nn, y_train_nn, units=config['simpleRNN_units']):
+        super(SimpleRNN, self).__init__()
         self.n_timesteps = None #X_train_nn.shape[1]
         self.n_channels = X_train_nn.shape[2]
         self.output_units = y_train_nn.shape[-1]
-        self.units = units 
+        self.units = units
+        self.loss = "mean_squared_error"
         print(f"input_shape = {(self.n_timesteps, self.n_channels)} | output_units = {self.output_units}")
-                
-
+               
 
     def build_model(self):
         """Метод формирования модели
@@ -50,6 +51,13 @@ class simpleRNN():
         print(model.summary())
         return model
     
+    def compile(self, model):
+        compiled_model = model.compile(
+            loss="mean_squared_error", 
+            metrics=[f1],
+            optimizer=tf.keras.optimizers.Adam(), # по умолчанию learning rate=10e-3
+        )
+        return compiled_model
     
 
 class LSTM():
@@ -62,6 +70,7 @@ class LSTM():
     lstm_units - размерность модели
     """
     def __init__(self, X_train_nn, x_trn_pred_dict):
+        super(LSTM, self).__init__()
         self.n_timesteps = X_train_nn.shape[1]
         self.n_channels = X_train_nn.shape[2]
         self.output_units = np.mean(x_trn_pred_dict[3], axis=0).shape[-1]  # среднее предсказание от 3-х моделей SRNN
