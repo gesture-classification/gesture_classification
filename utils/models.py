8ab31496f68aedf3cca65145c1a07ac81a0d6212
Alex1iv <ivanov.politehnik@gmail.com>
2022-12-25 09:46:19 +0600

    add lstm classs

--- utils/models.py
+++ utils/models.py
@@ -21,7 +21,7 @@ class SimpleRNN(Model):
     units - размерность модели из конфига 
     """    
     def __init__(self, X_train_nn, y_train_nn, units=config['simpleRNN_units']):
-        
+
         super(SimpleRNN, self).__init__()
         #------- параметры ------------
         self.n_timesteps = None #X_train_nn.shape[1]
@@ -40,16 +40,16 @@ class SimpleRNN(Model):
         
         #self.model = self.build_model()
         #self.model = self.compile()
-        #self.fit_model = self.fit()     
+     
         
     
-    def call(self):
-        x = self.input_layer
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.output_layer(x)
-        return x # returns results of Output Layer
+    # def call(self):
+    #     x = self.input_layer
+    #     x = self.layer1(x)
+    #     x = self.layer2(x)
+    #     x = self.layer3(x)
+    #     x = self.output_layer(x)
+    #     return x # returns results of Output Layer
 
         
     def build_model(self):
@@ -64,29 +64,17 @@ class SimpleRNN(Model):
         return model
     
     
-    def compile(self):
-        self.model.compile(
-            loss="mean_squared_error", 
-            metrics=[f1],
-            optimizer='adam', # tf.keras.optimizers.Adam() по умолчанию learning rate=10e-3
-        )
-        
-    
-    # def fit(self, model, X_train_nn, y_train_nn, validation_split, callbacks):
-    #     fitted_model = model.fit(
-    #         X_train_nn, 
-    #         y_train_nn, 
-    #         validation_split, # validation_split изменяется в цикле
-    #         callbacks, #=callbacks(num_train) # остальные параметры - смотри в functions.py
-    #         epochs=500,
-    #         verbose=1,
+    # def compile(self):
+    #     self.model.compile(
+    #         loss="mean_squared_error", 
+    #         metrics=[f1],
+    #         optimizer='adam', # tf.keras.optimizers.Adam() по умолчанию learning rate=10e-3
     #     )
-    #     return fitted_model
         
-    
 
-class LSTM():
-    """Класс создаёт модель LSTM.
+
+class LSTM(Model):
+    """Класс создаёт модель LSTM, наследуя класс от tf.keras.
     Параметры:
     ----------
     n_timesteps (_int_) - кол-во временных периодов
@@ -95,37 +83,40 @@ class LSTM():
     lstm_units - размерность модели
     """
     def __init__(self, X_train_nn, x_trn_pred_dict):
+        
         super(LSTM, self).__init__()
+        #------- параметры ------------
         self.n_timesteps = X_train_nn.shape[1]
         self.n_channels = X_train_nn.shape[2]
         self.output_units = np.mean(x_trn_pred_dict[3], axis=0).shape[-1]  # среднее предсказание от 3-х моделей SRNN
         self.lstm_units = config['lstm_units']
-        print(f"input_shape = {(self.n_timesteps, self.n_channels)} | output_units = {self.output_units}")
-
-
-    def build_model_LSTM(self):
-        """Метод формирования модели
-        """
-        input_channels = x = tf.keras.layers.Input(shape=(self.n_timesteps, self.n_channels))
-
-        x = tf.keras.layers.BatchNormalization()(x)
         
+        #-------- слои модели ----------------
+        self.input_channels = x = tf.keras.layers.Input(shape=(self.n_timesteps, self.n_channels))
+        x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.LSTM(units=self.lstm_units, return_sequences=True)(x)
         x = tf.keras.layers.LSTM(units=self.lstm_units, return_sequences=True)(x)
         x = tf.keras.layers.LSTM(units=self.lstm_units, return_sequences=True)(x)
         x = tf.keras.layers.LSTM(units=self.lstm_units, return_sequences=True, dropout=0.5)(x) 
-
         x = tf.keras.layers.Dense(256, activation='relu')(x)
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.Dropout(0.5)(x)
 
-        output = tf.keras.layers.Dense(units=self.output_units, activation='softmax')(x)
+        self.output_channels = tf.keras.layers.Dense(units=self.output_units, activation='softmax')(x)
+        
+        print(f"input_shape = {(self.n_timesteps, self.n_channels)} | output_units = {self.output_units}")
 
+        
+
+    def build_model_LSTM(self):
+        """Метод формирования модели
+        """
         model_lstm = tf.keras.Model(
-            inputs=input_channels,
-            outputs=output,
+            inputs=self.input_channels,
+            outputs=self.output_channels,
             name="model_LSTM"
         )        
-        print(model_lstm.summary())
+        model_lstm.summary()
         
         return model_lstm
+    
